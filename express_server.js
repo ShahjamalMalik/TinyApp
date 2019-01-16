d6f152cad8b00a5e15009e0f88d6b26364da49ce
ShahjamalMalik <shahjamalmalik@gmail.com>
2019-01-16 15:01:16 +0000

    Added a route for /urls, and linked an empty EJS file to it

---
+++ express_server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var PORT = 8080;
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
