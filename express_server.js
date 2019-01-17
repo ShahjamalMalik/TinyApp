7ef772f5db0ec6e5467077b8ad6c7788333ecf45
ShahjamalMalik <shahjamalmalik@gmail.com>
2019-01-17 18:21:56 +0000

    Created a new users object that holds the information of the users, created a registration handler with an endpoint (/register).

--- express_server.js
+++ express_server.js
@@ -13,6 +13,18 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
 
 
 app.listen(PORT, () => {
@@ -85,6 +97,21 @@ app.post("/login", (req, res) => {
   let value = req.body.email;
   res.cookie("user_id", value)
   res.redirect("/urls");
+});
+
+app.post("/register", (req,res) => {
+  var id = generateRandomString();
+  let foundUser = null;
+
+  users[id] =  {
+    id,
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  res.cookie("user_id", id);
+  res.redirect("/urls");
+  console.log(users);
 });
 
 function generateRandomString() {
